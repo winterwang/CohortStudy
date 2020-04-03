3723f954c1e99e750f22c7198368b82cb9e0b1de
wangcc.me <abelardccwang@gmail.com>
2020-04-03 15:13:22 +0900

    updated page 4 fig

---
+++ fig.R
@@ -0,0 +1,11 @@
+library(DiagrammeR)
+mermaid("
+graph LR
+  A((Population))==>B[People <br> without<br> disease]
+  B==>C[Exposed]
+  B==>D[Not Exposed]
+  C==>E[Disease]
+  C==>F[No Disease]
+  D==>G[Disease]
+  D==>H[No Disease]
+", width = 1000, height = 500)
